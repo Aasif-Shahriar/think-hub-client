8841af2d1a47839358aae21aafbfbd1fa39b0704
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-07-19 14:51:57 +0600

    Update: loading animation and data

--- src/components/loding/LoadingBar.jsx
+++ src/components/loding/LoadingBar.jsx
@@ -1,48 +1,14 @@
-import { useState } from "react";
-import { BarLoader } from "react-spinners";
-
-const override = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
+import Lottie from "lottie-react";
+import loading from "../../../public/Ripple loading animation.json";
 
 const LoadingBar = () => {
-  const [loading, setLoading] = useState(true);
-  const [color, setColor] = useState("#0000ff");
-
   return (
-    <div
-      className="sweet-loading"
-      style={{ textAlign: "center", padding: "1rem" }}
-    >
-      <button
-        type="button"
-        onClick={() => setLoading(!loading)}
-        style={{ marginBottom: "1rem" }}
-      >
-        Toggle Loader
-      </button>
-
-      <div style={{ marginBottom: "1rem" }}>
-        <label>
-          Loader Color:&nbsp;
-          <input
-            type="text"
-            value={color}
-            onChange={(e) => setColor(e.target.value)}
-            placeholder="Color of the loader"
-          />
-        </label>
-      </div>
-
-      <BarLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+    <div className="min-h-screen flex justify-center items-center">
+      <Lottie
+        className="w-[200px]"
+        animationData={loading}
+        loop={true}
+      ></Lottie>
     </div>
   );
 };
